72215579ee013f08c2e470d2394a4b0349b9e3a2
analitik9 <analitik9@gmail.com>
2019-03-01 11:15:12 +0300

    Initial Commit

---
+++ MillionaireDA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MillionaireDA
+//
+//  Created by Denis Evdokimov on 01/03/2019.
+//  Copyright © 2019 Denis Evdokimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
